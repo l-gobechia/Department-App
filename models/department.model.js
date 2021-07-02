d7914290bdcf4b91b57fa92c6b098a4634f11034
l-gobechia <l.gobechia.exceedteam@gmail.com>
2021-07-02 13:12:55 +0400

    Adding department model router and controller

---
+++ models/department.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const DepartmentSchema = mongoose.Schema({
+    departmentName: String,
+    departmentDescription: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Department', DepartmentSchema);

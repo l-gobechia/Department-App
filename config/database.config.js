2ab87e2b849ee6276e54aab422f07b942c2ec743
l-gobechia <l.gobechia.exceedteam@gmail.com>
2021-07-09 13:03:10 +0400

    changed database.config

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+    url: `mongodb+srv://${process.env.DB_NAME}:<${process.env.DB_PASS}>@${process.env.DB_NAME}.7iwby.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 }
